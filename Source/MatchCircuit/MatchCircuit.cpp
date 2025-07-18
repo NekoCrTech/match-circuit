742e12f1b8e05857f6fd0c4186e1e65b25805027
nektpol <nekocreativetechnologies@gmail.com>
2025-07-18 10:32:20 +0300

    Initialize Project

---
+++ Source/MatchCircuit/MatchCircuit.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MatchCircuit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MatchCircuit, "MatchCircuit" );
